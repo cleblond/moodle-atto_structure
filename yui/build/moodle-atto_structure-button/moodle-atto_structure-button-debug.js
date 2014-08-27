4af6ed069c326ba4d5b448cc2803d2deade28ec5
Carl LeBlond <carl.leblond@iup.edu>
2014-08-27 16:21:01 -0400

    fixed many bugs and cleaned up code

--- yui/build/moodle-atto_structure-button/moodle-atto_structure-button-debug.js
+++ yui/build/moodle-atto_structure-button/moodle-atto_structure-button-debug.js
@@ -49,23 +49,16 @@ var CSS = {
     };
 
 var TEMPLATE = '' +
-//    '<link type="text/css" rel="stylesheet" href="http://localhost/marvinjs-14.7.7/css/doc.css" />' +
-//    '<link type="text/css" rel="stylesheet" href="http://localhost/marvinjs-14.7.7/js/lib/rainbow/github.css" />' +
-//    '<script type="text/javascript" src="http://localhost/marvinjs-14.7.7/js/lib/rainbow/rainbow-custom.min.js"></script>' +
-//    '<script type="text/javascript" src="http://localhost/marvinjs-14.7.7/gui/gui.nocache.js"></script>' +
-//    '<script type="text/javascript" src="http://localhost/marvinjs-14.7.7/js/promise-0.1.1.min.js"></script>' +
-//    '<script type="text/javascript" src="http://localhost/marvinjs-14.7.7/js/marvinjslauncher.js"></script>' +
     '<form class="atto_form">' +
         '<div id="{{elementid}}_{{innerform}}" class="mdl-align">' +
             '<strong>{{get_string "instructions" component}}</strong>' +
             '<table><tr><td><label for="{{elementid}}_{{WIDTHCONTROL}}">{{get_string "width" component}}</label></td>' +
-            '<td><input class="{{CSS.WIDTHCONTROL}}" size="6" id="{{elementid}}_{{WIDTHCONTROL}}" name="{{elementid}}_{{WIDTHCONTROL}}" value="{{defaultwidth}}" /></td><td></td></tr>' +
+            '<td><input class="{{CSS.WIDTHCONTROL}}" size="6" id="{{elementid}}_{{WIDTHCONTROL}}" name="{{elementid}}_{{WIDTHCONTROL}}"'+
+            'value="{{defaultwidth}}" /></td><td></td></tr>' +
             '<tr><td><label for="{{elementid}}_{{HEIGHTCONTROL}}">{{get_string "height" component}}</label></td>' +
             '<td><input class="{{CSS.HEIGHTCONTROL}}" size="6" id="{{elementid}}_{{HEIGHTCONTROL}}" name="{{elementid}}_{{HEIGHTCONTROL}}" value="{{defaultheight}}" /><td>' +
             '<button class="{{CSS.INPUTSUBMIT}}">{{get_string "insert" component}}</button></td></tr></table>' +
         '</div>' +
-        'icon: {{clickedicon}}'  +
-//        '<iframe src="http://localhost/marvinjs-14.7.7/editor.html" id="MSketch" class="sketcher-frame" height="510px"></iframe>' +
     '</form>';
 
 Y.namespace('M.atto_structure').Button = Y.Base.create('button', Y.M.editor_atto.EditorPlugin, [], {
@@ -92,16 +85,11 @@ Y.namespace('M.atto_structure').Button = Y.Base.create('button', Y.M.editor_atto
             return;
         }
 
-
-
         // If we don't have the capability to view then give up.
         if (this.get('disabled')){
             return;
         }
 
-        //var twoicons = ['icon'];
-
-        //Y.Array.each(twoicons, function(theicon) {
             // Add the structure icon/buttons
 
             this.addButton({
@@ -111,21 +99,9 @@ Y.namespace('M.atto_structure').Button = Y.Base.create('button', Y.M.editor_atto
                 callback: this._displayDialogue,
                 callbackArgs: 'icon'
             });
-        //}, this);
 
     },
 
-    /**
-     * Get the id of the flavor control where we store the ice cream flavor
-     *
-     * @method _getFlavorControlName
-     * @return {String} the name/id of the flavor form field
-     * @private
-     */
-/*
-    _getFlavorControlName: function(){
-        return(this.get('host').get('elementid') + '_' + WIDTHCONTROL);
-    }, */
 
      /**
      * Display the structure Dialogue
@@ -143,11 +119,6 @@ Y.namespace('M.atto_structure').Button = Y.Base.create('button', Y.M.editor_atto
             width: '768px',
             focusAfterHide: clickedicon
         });
-        //dialog doesn't detect changes in width without this
-        //if you reuse the dialog, this seems necessary
-      /*  if(dialogue.width !== width + 'px'){
-            dialogue.set('width',width+'px');
-        }  */
 
         var iframe = Y.Node.create('<iframe></iframe>');
         iframe.setStyles({
@@ -158,9 +129,6 @@ Y.namespace('M.atto_structure').Button = Y.Base.create('button', Y.M.editor_atto
         iframe.setAttribute('src', this._getIframeURL());
         iframe.setAttribute('id', 'sketch');
         iframe.setAttribute('data-toolbars', 'education');
-
-
-        //iframe.setAttribute('src', this._getIframeURL());
 
         //append buttons to iframe
         var buttonform = this._getFormContent(clickedicon);
@@ -199,246 +167,143 @@ Y.namespace('M.atto_structure').Button = Y.Base.create('button', Y.M.editor_atto
 
         this._form = content;
         this._form.one('.' + CSS.INPUTSUBMIT).on('click', this._getImgURL, this);
-        //this._form.one('.' + CSS.INPUTSUBMIT).on('click', this._getImage, this);
         return content;
     },
 
 
 
     _getIframeURL: function() {
-        //return M.cfg.wwwroot + '/lib/editor/atto/plugins/structure/dialog/marvinjs.php';
-
-
 
         return this.get('path')+'/editor.html';
 
-/* + 
-          'itemid='+ this._itemid + '&recorder=' + therecorder + '&usewhiteboard=' + this._usewhiteboard  + 
-          '&updatecontrol=' + this._getFilenameControlName();  */
     },
 
 
 
 
-	_uploadFile: function(filedata, recid) {
-		
-		var xhr = new XMLHttpRequest();
-		var ext="png";
-		
+    _uploadFile: function(filedata, recid, filename) {
+        
+        var xhr = new XMLHttpRequest();
+        var ext="png";
 
-			// file received/failed
-			xhr.onreadystatechange = (function(mfp){return function(e) {
-			        //console.log("in onreadystatechange");
-				if (xhr.readyState == 4 ) {
-                                        //console.log("ready state 4");
-					/*if(progress){
-						progress.className = (xhr.status == 200 ? "success" : "failure");
-					}*/
-					if(xhr.status==200){
-						var resp = xhr.responseText;
-						var start= resp.indexOf("success<error>");
-						if (start<1){return;}
-						var end = resp.indexOf("</error>");
-						var filename= resp.substring(start+14,end);
-					        //console.log("filename" + filename);
-						//invoke callbackjs if we have one, otherwise just update the control(default behav.)
-						//if(opts['callbackjs'] && opts['callbackjs']!=''){ 
-						//	var callbackargs  = new Array();
-						//	callbackargs[0]=opts['recorderid'];
-						//	callbackargs[1]='filesubmitted';
-						//	callbackargs[2]=filename;
-						//	callbackargs[3]=opts['updatecontrol'];
-							//window[opts['callbackjs']](callbackargs);
-                                                        //console.log("ready state 4");
-						//	mfp.Output(recid, "File saved successfully.");
-						//	mfp.executeFunctionByName(opts['callbackjs'],window,callbackargs);
-							
-						//}else{
-							//console.log("ready state 4");
-														
-							//mfp.Output(recid, "File saved successfully.");
-                                                        //console.log ("File uploaded succesfully");
-							//var upc = mfp.getbyid(mfp.getbyid(recid + "_updatecontrol").value);
-							//if(!upc){upc = mfp.getbyidinparent(mfp.getbyid(recid + "_updatecontrol").value);}
-							//upc.value=filename;
-						//}
-					}else{
-						//mfp.Output(recid, "File could not be uploaded.");
-						//console.log ("File could not be uploaded!");
-					}
-				}
-			}})(this);
+            // file received/failed
+            xhr.onreadystatechange = (function(){return function() {
+                    //console.log("in onreadystatechange");
+                if (xhr.readyState === 4 ) {
+                    //console.log("ready state 4");
+                    if(xhr.status===200){
+                        var resp = xhr.responseText;
+                        var start= resp.indexOf("success<error>");
+                        if (start<1){return;}
                         
-			var params = "datatype=uploadfile";
-			//We must URI encode the base64 filedata, because otherwise the "+" characters get turned into spaces
-			//spent hours tracking that down ...justin 20121012
-			params += "&paramone=" + encodeURIComponent(filedata);
-			params += "&paramtwo=" + ext;
-			params += "&paramthree=image";
-			params += "&requestid=" + this._filename;
-			params += "&contextid="+this._usercontextid;
-			params += "&component=user";
-			params += "&filearea=draft";
-			params += "&itemid=" + this._itemid;
-			//console.log("params="+params);
-			xhr.open("POST", M.cfg.wwwroot+"/lib/editor/atto/plugins/structure/structurefilelib.php", true);
-			xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-			xhr.setRequestHeader("Cache-Control", "no-cache");
-			xhr.setRequestHeader("Content-length", params.length);
-			xhr.setRequestHeader("Connection", "close");
-			xhr.send(params);
-
-
-	},
-
-
-
-
-
+                    }
+                }
+            };})(this);
+            var params = "datatype=uploadfile";
+            params += "&paramone=" + encodeURIComponent(filedata);
+            params += "&paramtwo=" + ext;
+            params += "&paramthree=image";
+            params += "&requestid=" + filename;
+            params += "&contextid="+this._usercontextid;
+            params += "&component=user";
+            params += "&filearea=draft";
+            params += "&itemid=" + this._itemid;
+            //console.log("params="+params);
+            xhr.open("POST", M.cfg.wwwroot+"/lib/editor/atto/plugins/structure/structurefilelib.php", true);
+            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            xhr.setRequestHeader("Cache-Control", "no-cache");
+            xhr.setRequestHeader("Content-length", params.length);
+            xhr.setRequestHeader("Connection", "close");
+            xhr.send(params);
+    },
 
   _getImgURL: function(e) {
 
         e.preventDefault();
-    var dialog = this.getDialogue({
+        this.getDialogue({
             focusAfterHide: null
-        }).hide();  
-
+        }).hide();
 
                             var widthcontrol = this._form.one(SELECTORS.WIDTHCONTROL);
-			    var newwidth = '';
+                var newwidth = '';
                             // If no file is there to insert, don't do it.
                             if (!widthcontrol.get('value')) {
                                 newwidth = this.get('defaultwidth');
                             } else {
                                 newwidth = widthcontrol.get('value');
-                            } 
-
-
+                            }
 
                             var heightcontrol = this._form.one(SELECTORS.HEIGHTCONTROL);
-			    var newheight = '';
+                var newheight = '';
                             // If no file is there to insert, don't do it.
                             if (!heightcontrol.get('value')) {
                                 newheight = this.get('defaultheight');
                             } else {
                                 newheight = heightcontrol.get('value');
-                            } 
+                            }
 
 
-
-
-
-
-//console.log(this);
-
-       // console.log("path="+this.get('path'));
-
-
-var referringpage = this;
-Y.Get.js([this.get('path') + '/gui/gui.nocache.js', this.get('path') + '/js/marvinjslauncher.js', 
-this.get('path') + '/js/promise-0.1.1.min.js'], function (err) {
+        var referringpage = this;
+        Y.Get.js([this.get('path') + '/gui/gui.nocache.js', this.get('path') + '/js/marvinjslauncher.js',
+        this.get('path') + '/js/promise-0.1.1.min.js'], function (err) {
     if (err) {
-    //    Y.log('Error loading JS: ' + err[0].error, 'error');
         return;
     }
-    //Y.log('file.js loaded successfully!');
-
-
-               //console.log(referringpage);
-
-		//marvin.onReady(function() {
-                //
-		//console.log("marvin is ready");
-		//});
-                                            
 
            var marvinController;
             MarvinJSUtil.getEditor("#sketch").then(
                 function(sketcherInstance) {
                     marvinController = new MarvinControllerClass(
                         sketcherInstance);
-            
+
                     exportPromise = marvinController.sketcherInstance.exportStructure("mrv", null);
 
                         exportPromise.then(function(source) {
 
 
-				var imgsettings = {
-				 'carbonLabelVisible' : false,
-				 'chiralFlagVisible' : true,
-				 'valenceErrorVisible' : true,
-				 'lonePairsVisible' : true,
-				 'implicitHydrogen' : "TERMINAL_AND_HETERO",
-				// 'displayMode' : "WIREFRAME",
-				 'width' : newwidth,
-				 'height' : newheight
-				 }
+                var imgsettings = {
+                 'carbonLabelVisible' : false,
+                 'chiralFlagVisible' : true,
+                 'valenceErrorVisible' : true,
+                 'lonePairsVisible' : true,
+                 'implicitHydrogen' : "TERMINAL_AND_HETERO",
+                // 'displayMode' : "WIREFRAME",
+                 'width' : newwidth,
+                 'height' : newheight
+                 };
+                    imgURL = marvin.ImageExporter.mrvToDataUrl(source, "image/png", imgsettings);
 
+                    var filename = new Date().getTime();
 
-                           // console.log(SELECTORS.WIDTHCONTROL);
-                           // console.log('newwidth=' + referringpage._form.one(SELECTORS.WIDTHCONTROL).get('value'));
-
-                            //convert to image
-		            imgURL = marvin.ImageExporter.mrvToDataUrl(source, "image/png", imgsettings);
-
-                          referringpage._uploadFile(imgURL, "1");
+                          referringpage._uploadFile(imgURL, "1", filename);
 
 
 
-                   var thefilename = "upfile_"+referringpage._filename+".png";
+                   var thefilename = "upfile_"+filename+".png";
 
                    var wwwroot = M.cfg.wwwroot;
+           // It will store in mdl_question with the "@@PLUGINFILE@@/myfile.mp3" for the filepath.
+               var filesrc =wwwroot+'/draftfile.php/'+  referringpage._usercontextid +'/user/draft/'+referringpage._itemid+'/' + thefilename;
 
-	           //var mediahtml='';
-           
-		   // It will store in mdl_question with the "@@PLUGINFILE@@/myfile.mp3" for the filepath.
-			   var filesrc =wwwroot+'/draftfile.php/'+  referringpage._usercontextid +'/user/draft/'+referringpage._itemid+'/' + thefilename;
+                    divContent ="<img name=\"pict\" src=\"" + filesrc + "\" alt=\"MarvinJS PNG\"/>";
 
-////
-
-		            divContent ="<div class=\"marvinjs-image\"><img name=\"pict\" src=\"" + filesrc + "\" alt=\"MarvinJS PNG\"/></div>";
-
-                            
-
-			referringpage.editor.focus
-		        referringpage.get('host').insertContentAtFocusPoint(divContent);
-		        //this.get('host').insertContentAtFocusPoint(divContent);
-		        referringpage.markUpdated();
-
-
+            referringpage.editor.focus();
+                referringpage.get('host').insertContentAtFocusPoint(divContent);
+                referringpage.markUpdated();
                     });
-                   //console.log("here2");
-                   //console.log(imgURL._detail);
-                   //return imgURL2;
                 });
-            var MarvinControllerClass = (function() {
+            MarvinControllerClass = (function() {
                 function MarvinControllerClass(
                     sketcherInstance
                 ) {
                     this.sketcherInstance =
                         sketcherInstance;
-                    this.init();
                 }
-                MarvinControllerClass.prototype.init =
-                    function init() {
-                        this.sketcherInstance.setDisplaySettings({
-                            "cpkColoring": true,
-                            "lonePairsVisible": true,
-                            "toolbars": "education"
-                        });
-                    };
                 return MarvinControllerClass;
             }());
-
-//console.log("here3 = "+imgURL);
-//return imgURL;
 });
 
-//return imgURL;
-
-
 }
-
 
 }, { ATTRS: {
         disabled: {
