a0a75b44b99d008ac7684a74fd93f54a6d1ff3c6
Carl LeBlond <carl.leblond@iup.edu>
2014-08-31 09:46:03 -0400

    revised version

--- version.php
+++ version.php
@@ -27,6 +27,6 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version   = 2014083000;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2013110500;        // Requires this Moodle version.
 $plugin->component = 'atto_structure';  // Full name of the plugin (used for diagnostics).
-$plugin->maturity  = MATURITY_BETA;
-$plugin->release   = '1.0.0 (Build 2014042800)';
+$plugin->maturity  = MATURITY_STABLE;
+$plugin->release   = '1.0 (Build 2014083000)';
 
